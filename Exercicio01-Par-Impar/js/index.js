97d6b5a5e68aca4195fda290db6f7e328d9d3010
bumyy32 <bumyy32@gmail.com>
2021-12-08 09:49:40 -0300

    basico feito
    Add html e css e modelo do site, inicio do js

---
+++ Exercicio01-Par-Impar/js/index.js
@@ -0,0 +1,9 @@
+var botao = document.querySelector("#btn-calcular");
+
+botao.addEventListener("click", function() {
+    var num1 = parseInt(document.querySelector("#numero1").value);
+    var num2 = parseInt(document.querySelector("#numero2").value);
+    var num3 = parseInt(document.querySelector("#numero3").value);
+
+    
+})
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# avalia-o-logica-programa-ao
+# avaliaco-logica-programacao
 Avaliação do dia 08/12 sobre lógica de programação
