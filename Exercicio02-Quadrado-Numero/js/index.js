4f55aa9aa76a43b180a24cd158dc03679c32c70f
bumyy32 <bumyy32@gmail.com>
2021-12-08 10:28:06 -0300

    Finalizado ex. ao quadrado
    mt facil slc

--- Exercicio02-Quadrado-Numero/js/index.js
+++ Exercicio02-Quadrado-Numero/js/index.js
@@ -1,29 +1,9 @@
 var botao = document.querySelector("#btn-calcular");
 
 botao.addEventListener("click", function() {
-    var num1 = parseInt(document.querySelector("#numero1").value);
-    var num2 = parseInt(document.querySelector("#numero2").value);
-    var num3 = parseInt(document.querySelector("#numero3").value);
+    var num1 = parseInt(document.querySelector("#numero").value);
 
-    if (num1 % 2 == 0) {
-        document.querySelector("#resultado1").innerHTML = "O resultado é: " + num1 * 3
-    }
-    else{
-        document.querySelector("#resultado1").innerHTML = "O resultado é: " + (num1 + 7);
-    }
+    var calculo = num1 * num1;
 
-    if (num2 % 2 == 0) {
-        document.querySelector("#resultado2").innerHTML = "O resultado é: " + num2 * 3
-    }
-    else{
-        document.querySelector("#resultado2").innerHTML = "O resultado é: " + (num2 + 7);
-    }
-
-    if (num3 % 3 == 0) {
-        document.querySelector("#resultado3").innerHTML = "O resultado é: " + num3 * 3
-    }
-    else{
-        document.querySelector("#resultado3").innerHTML = "O resultado é: " + (num3 + 7);
-    }
-
+    document.querySelector("#resultado").innerHTML = "O quadrado do número " + num1 + " é: " + calculo
 })
